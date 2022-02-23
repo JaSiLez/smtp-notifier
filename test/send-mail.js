d7309e1e8858ca62b67a7cebc7c5845f1dcd06aa
Jaime Silva <jaime.silva@gruposantander.com>
2022-02-23 16:00:09 +0100

    add secrets to testing

--- test/send-mail.js
+++ test/send-mail.js
@@ -1,11 +1,13 @@
+const smtpUser = JSON.parse(process.env['SMTP_USER'])
+const smtpPass = JSON.parse(process.env['SMTP_PASSWORD'])
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.yourdomain.email",
   port: 587,
   secure: false,
   auth: {
-    user: SMTP_USER
-    pass: SMTP_PASSWORD
+    user: smtpUser
+    pass: smtpPass
   }
 });
 // send mail with defined transport object
