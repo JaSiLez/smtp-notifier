cac4931afc850693fd2616d66b5c00252e6f5707
Jaime Silva González <64492375+JaSiLez@users.noreply.github.com>
2022-02-25 09:54:46 +0100

    Fix codeaudicy (#7)
    * cheking audicy
    
    * add eslintrc
    
    * add ruleset
    
    * add folder exception
    
    * try
    
    * update actions sha
    
    * Try 1
    
    * exception folder node_module
    
    * add jshintrc
    
    * fix semicolon
    
    * fix semicolon
    
    * prepare workflow flow
    
    * add semantic release drafter config
    
    * fix release drafter config
    
    * upgrade dependabot fixes prs
    
    * upgrade dotenv node_module
    
    * add markdownlint.json

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    parser: "babel-eslint",
+    env: {
+      browser: true,
+      node: true,
+      es6: true,
+      jest: true,
+    }, 
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:jsx-a11y/recommended"
+    ],
+    plugins: [
+      "react",
+      "react-hooks",
+      "jsx-a11y",
+    ],
+    rules: {
+      semi: [2, "never"],
+      strict: 0,
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn"
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
